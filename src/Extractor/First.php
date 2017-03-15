b7f05b7aa9e674ccc95b639fc07fe57d13bfbcb6
ssnepenthe <ssnepenthe@users.noreply.github.com>
2017-03-15 16:12:30 -0700

    Ensure First extractor truly only returns the first value.

--- src/Extractor/First.php
+++ src/Extractor/First.php
@@ -8,6 +8,8 @@ class First extends BaseExtractor
 {
     protected function doExtract(Crawler $crawler) : array
     {
-        return $crawler->first()->extract($this->attr);
+        $values = $crawler->first()->extract($this->attr);
+
+        return [$this->getFirstValue($values)];
     }
 }
